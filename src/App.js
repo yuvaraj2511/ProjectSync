4d019e86b07b2dc7c6575655a3cc07faa1cca363
yuvaraj2511 <yuvaraj25112003@gmail.com>
2023-09-16 10:47:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
+import BrowseProjects from './BrowseProjects';
+import ResearchPapers from './ResearchPapers';
+import EventCalendar from './EventCalendar';
+import Dashboard from './Dashboard';
+import Dashboard_Ins from './Dashboard_ins';
+// import Chat from './Chat';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/browseprojects" element={<BrowseProjects />} />
+        <Route path="/researchpapers" element={<ResearchPapers />} />
+        <Route path="/eventcalendar" element={<EventCalendar />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard_ins" element={<Dashboard_Ins />} />
+        {/* <Route path="/chat" element={<Chat />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
